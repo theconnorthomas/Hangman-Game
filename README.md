17c26f897df4e593ddb62ab02d7f891f665c2774
Connor Thomas <connorthomas@connors-mbp.attlocal.net>
2018-02-16 22:03:14 -0600

    added more info to readme and made changes to the js

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # Hangman-Game
 Hangman game Javascript homework
+
+My hangman game is losely based off of wargames and retro commandline interface. 
+
+Type keys one at a time on the keybroad totry in fill in the correct military callsign and deactivate the missiles that have been launched.
+
--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -2,11 +2,11 @@ var phrases = ['echo tango alfa', 'oscar mike', 'whiskey tango foxtrot', 'lima c
 
 var allGuesses = [];
 
-var wrongGuesses = [];
+var incorrectGuesses = [];
 
 var underscores = [];
 
-var lives = 10;
+var lives = 11;
 
 var wins = 0;
 
@@ -14,17 +14,20 @@ var losses = 0;
 
 var currentPhrase = [chosePhrase()];
 
+function generateIndex(index) {
 
-function generateIndex(i) {
-    return Math.floor(Math.random() * i);
+    return Math.floor(Math.random() * index);
+
 }
 
 function chosePhrase() {
+
     return phrases[generateIndex(phrases.length)]
+
 }
 
+function inputs() {
 
-function spaces() {
     for (i = 0; i < currentPhrase[0].length; i++) {
         if (currentPhrase[0][i] === ' ') {
             underscores.push(' ');
@@ -33,91 +36,82 @@ function spaces() {
             underscores.push('_');
         }
     };
+
 }
 
-
 function initialize() {
-    document.getElementById('spaces').innerHTML = underscores.join('');
 
-    document.getElementById('allGuesses').innerHTML = wrongGuesses;
+    inputs();
 
-    document.getElementById('lives').innerHTML = 'Incorrect inputs remaining before failure: ' + lives;
+    correctGuess();
+
+    incorrectGuess();
 
     document.getElementById('wins').innerHTML = 'Missiles Deactivated: ' + wins;
 
     document.getElementById('losses').innerHTML = 'Failed Logins: ' + losses;
 
+}
 
+function correctGuess() {
 
-
-
-};
-
-function guessCorrect() {
-
-    document.getElementById('spaces').innerHTML = underscores.join('');
-
-};
-
-function guessIncorrect() {
-
-    lives--;
-
-    document.getElementById('lives').innerHTML = 'Incorrect inputs remaining before failure: ' + lives;
-
-    document.getElementById('allGuesses').innerHTML = 'Incorrect inputs: ' + wrongGuesses;
+    document.getElementById('inputs').innerHTML = underscores.join('');
 
 }
 
-function gameWon() {
+function winner() {
 
     wins++;
 
     document.getElementById('wins').innerHTML = 'Missiles Deactivated: ' + wins;
 
-    document.getElementById('status').innerHTML = 'PASSPHRASE CORRECT, MISSILE SYSTEM DEACTIVATED';
+    document.getElementById('sitRep').innerHTML = 'PASSWORD CORRECT, MISSILE SYSTEM DEACTIVATED!';
 
-};
+}
 
-function gameLost() {
+function incorrectGuess() {
 
+    lives--;
+
+    document.getElementById('lives').innerHTML = 'Incorrect inputs remaining before failure: ' + lives;
+
+    document.getElementById('allGuesses').innerHTML = 'Incorrect inputs: ' + incorrectGuesses;
+
+}
+
+function loser() {
 
     losses++;
 
     document.getElementById('losses').innerHTML = 'Failed Logins: ' + losses;
 
+    document.getElementById('sitRep').innerHTML = 'INVALID PASSWORD, TRY AGAIN!';
 
-    document.getElementById('status').innerHTML = 'INVALID INPUT, TRY AGAIN' + gameLost;
-
-};
-
+}
 
 function reinitialize() {
 
-
     underscores = [];
-
-    currentPhrase.splice(0, 1, chosePhrase());
-
-    spaces();
-    document.getElementById('spaces').innerHTML = underscores.join('');
-
-    lives = 10;
-    document.getElementById('lives').innerHTML = 'Incorrect inputs remaining before failure: ' + lives;
-
-    wrongGuesses = [];
-
-    document.getElementById('allGuesses').innerHTML = wrongGuesses;
-
+    
     allGuesses = [];
+    
+    incorrectGuesses = [];
+    
+    lives = 11;
 
-};
+    inputs();
+    
+    correctGuess();
+    
+    incorrectGuess();
 
+    document.getElementById('allGuesses').innerHTML = incorrectGuesses;
 
-spaces();
+}
+
 initialize();
 
-document.onkeyup = function (e) {
+document.onkeyup = function(e) {
 
     var guess = e.key.toLowerCase();
 
@@ -127,11 +121,9 @@ document.onkeyup = function (e) {
         };
     };
 
-
     if (currentPhrase[0].toLowerCase().indexOf(guess) > -1) {
 
         allGuesses.push(guess);
-
 
         for (i = 0; i < currentPhrase[0].length; i++) {
 
@@ -139,32 +131,31 @@ document.onkeyup = function (e) {
                 underscores.splice(i, 1, guess);
             };
 
-            guessCorrect();
-
+            correctGuess();
         };
 
     }
 
     else {
 
-        wrongGuesses.push(guess);
-
         allGuesses.push(guess);
+        
+        incorrectGuesses.push(guess);
 
-        guessIncorrect();
+        incorrectGuess();
 
     };
 
     if (underscores.join('') === currentPhrase[0].toLowerCase()) {
 
-        gameWon();
+        winner();
 
         reinitialize();
     }
 
-    if (lives <= 0) {
+    if (lives < 1) {
 
-        gameLost();
+        loser();
 
         reinitialize();
     };
